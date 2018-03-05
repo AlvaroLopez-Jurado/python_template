0525b2dc9667352c31d1b4f43cf23eadb6241a3e
Miguel Ingelmo <miguelingelmo@hotmail.com>
2018-03-05 11:20:14 +0100

    Prueba de git

--- tests/test_strings_examples.py
+++ tests/test_strings_examples.py
@@ -9,6 +9,17 @@ class TestStringsExamples(unittest.TestCase):
         result = StringsExamples.concat_strings(string1, string2)
         assert result == "holaadios"
 
+    def test_concat_number_string(self):
+        string1 = 123
+        string2 = "adios"
+        resutl = self.assertRaises(Exception ,StringsExamples.concat_strings(string1,string2))
+
+    def test_concat_string_number(self):
+        string1 = 123
+        string2 = "adios"
+        resutl = self.assertRaises(Exception ,StringsExamples.concat_strings(string1,string2))
+
+
 
 if __name__ == '__main__':
     unittest.main()
